37827d3f3da8908dcd1fcfd37c716190503c553f
Jesse Katabarwa <jessekatabarwa@gmail.com>
2026-06-30 20:41:38 +0900

    oasis disabled

--- src/components/PlayerWindow.vue
+++ src/components/PlayerWindow.vue
@@ -39,7 +39,7 @@ const tracks = [
   { title: 'f4r u', src: track7, disabled: false },
   { title: 'Never Mind', src: track1, disabled: false },
   { title: 'Syren Mars', src: track13, disabled: false },
-  { title: 'Oasis 101', src: track8, disabled: false },
+  { title: 'Oasis 101', src: track8, disabled: true },
   { title: 'bby', src: track12, disabled: false },
 
 ]
